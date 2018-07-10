fcf4161a052cf618dabfcf92d7b3170f94630290
javasusanto <herisusanto.id@gmail.com>
2018-07-10 14:13:26 +0700

    Exercises 13 Week 2

---
+++ exercise12.js
@@ -0,0 +1,19 @@
+// Logic Challenge - Konversi Menit
+function konversiMenit(minutes) {
+  var h = Math.floor(minutes / 60);
+  var m = minutes % 60;
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+
+// It has the form of: condition ? value-if-true : value-if-false
+// Think of the ? as "then" and : as "else". 
+
+  return h + ':' + m;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
