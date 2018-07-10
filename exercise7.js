7713207a0954b8e04c9729463601e99bc1120941
javasusanto <herisusanto.id@gmail.com>
2018-07-10 14:23:48 +0700

    Exercises 7 Week 2

---
+++ exercise7.js
@@ -0,0 +1,25 @@
+// Soal 1
+var rows=1;
+ for (rows;rows <= 5;rows +=1){
+   console.log ('*')
+ }
+
+// Soal 2
+var num = 4
+for (var i=0; i < num ;i++ ){
+  var asterix =''
+  for (var j=0; j < num;j++){
+  asterix +='*'
+  }
+  console.log (asterix)
+ }
+
+ // Soal 3
+
+ var rows2 = 5
+ var star = ''
+
+for (var y=0; y < rows2; y ++){
+  star += '*'
+  console.log (star)
+}
