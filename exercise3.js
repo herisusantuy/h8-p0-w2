9d7c28ecddc801a9d77c4a4a626a11ac00a05b37
javasusanto <herisusanto.id@gmail.com>
2018-07-25 11:10:17 +0700

    Revisi Exercise3 lagi hehehe..

--- exercise3.js
+++ exercise3.js
@@ -1,28 +1,18 @@
 // Soal 1
-var nama = 'Nina';
-var peran = 'Ksatria';
+var nama = 'Zero';
+var peran = 'Penyihir';
 
-if (nama == '' && peran == ''){
-  console.log ('Nama harus diisi!')
-}
-else if (nama != '' && peran ==''){
+if (peran === 'Ksatria'){
+  console.log ('Selamat data di Dunia Proxitia, '+nama)
+  console.log ('Halo '+peran+' '+nama+', kamu dapat menyerang dengan senjatamu!')
+} else if (peran === 'Tabib'){
+  console.log ('Selamat data di Dunia Proxitia, '+nama)
+  console.log ('Halo '+peran+' '+nama+', kamu akan membantu temanmu yang terluka.')
+} else if (peran === 'Penyihir'){
+  console.log ('Selamat data di Dunia Proxitia, '+nama)
+  console.log ('Halo '+peran+' '+nama+', ciptakan keajaiban yang membantu kemenanganmu!')
+} else if (nama !=='' && peran === ''){
   console.log ('Halo '+nama+', Pilih peranmu untuk memulai game!')
-}
-else if (nama == '' && peran !=''){
-  console.log ('Untuk memulai game nama Anda harus diisi!')
-}
-else if (nama == 'Nina' && peran =='Ksatria'){
-  console.log ('Selamat datang di dunia Proxyta, '+nama)
-  console.log ('Halo '+peran+' '+nama+ ', kamu dapat menyerang dengan senjatamu!')
-}
-else if (nama == 'Danu' && peran == 'Tabib'){
-  console.log ('Selamat datang di dunia Proxyta, '+nama)
-  console.log ('Halo '+peran+' '+nama+ ', kamu akan membantu temanmu yang terluka.')
-}
-else if (nama == 'Zero' && peran == 'Penyihir'){
-  console.log ('Selamat datang di dunia Proxyta, '+nama)
-  console.log ('Halo '+peran+' '+nama+ ', ciptakan keajaiban yang membantu kemenanganmu!')
-}
-else {
-  console.log ('Nama Anda belum terdaftar, silahkan hubungi Admin!')
-}
+} else if (nama ==='' && peran === ''){
+  console.log ('Nama harus diisi!')
+} 
